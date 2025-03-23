7bee7efd20e0cad4bb65838df0482494bc0f19ec
jinqiusky <108732806+jinqiusky@users.noreply.github.com>
2025-03-23 15:57:44 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -46,8 +46,9 @@ let time;
 const plugins = [
   { name: 'obfuscator', plugin: PluginObfuscator },
   { name: 'sojsonv7', plugin: PluginSojsonV7 },
+    { name: 'sojson', plugin: PluginSojson },
+
   { name: 'jsconfuser', plugin: PluginJsconfuser },
-  { name: 'sojson', plugin: PluginSojson },
   { name: 'awsc', plugin: PluginAwsc },
   { name: 'jjencode', plugin: PluginJjencode },
   { name: 'common', plugin: PluginCommon }, // Use common plugin last
@@ -76,8 +77,7 @@ if (processedCode !== sourceCode) {
   time = new Date();
   const header = [
     `//${time}`,
-    "//Base:<url id=\"cv1cref6o68qmpt26ol0\" type=\"url\" status=\"parsed\" title=\"GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code\" wc=\"2165\">https://github.com/echo094/decode-js</url>",
-    "//Modify:<url id=\"cv1cref6o68qmpt26olg\" type=\"url\" status=\"parsed\" title=\"GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密\" wc=\"741\">https://github.com/smallfawn/decode_action</url>"
+    "//DeCode Base By QQ:1229456555"
   ].join('\n');
 
   // Combine header and processed code
